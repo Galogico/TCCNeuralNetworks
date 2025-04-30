7838994a08c677af0fd8362decd3ebcb390ec597
Galogico <galogico@gmail.com>
2025-04-30 14:58:55 -0300

    colocando alguns comentarios

--- recog.py
+++ recog.py
@@ -1,19 +1,30 @@
 import torch
 import torchvision
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
 
-
-n_epochs = 3
+# quantas vezes a rede neural vai passar pelos dados de trino
+n_epochs = 1
+# Quantas imagens serão analisadas por iteração do treino
 batch_size_train = 64
+# Quantas imagens serão analisadas por iteração do teste
 batch_size_test = 1000
+# Quanto o programa mudará os pesos a cada geração
 learning_rate = 0.01
+#parâmetro do SGD pra rede neural aprender mais rápido
 momentum = 0.5
+# quanto tempo demora pra logar
 log_interval = 10
-
+# seed pra geração de números aleatórios
 random_seed = 1
+# Desativa o programa da NVIDIA pra rodar mais rápido em placa de vídeo, se você tiver, mude para True
 torch.backends.cudnn.enabled = False
 torch.manual_seed(random_seed)
 
+# dados de treino e teste colocados em um loader
 train_loader = torch.utils.data.DataLoader(
+   # pegando o dataset da internet
   torchvision.datasets.MNIST('/files/', train=True, download=True,
                              transform=torchvision.transforms.Compose([
                                torchvision.transforms.ToTensor(),
@@ -31,18 +42,15 @@ test_loader = torch.utils.data.DataLoader(
                              ])),
   batch_size=batch_size_test, shuffle=True)
 
-examples = enumerate(test_loader)
-batch_idx, (example_data, example_targets) = next(examples)
-
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-
+# classe da NN
 class Net(nn.Module):
+    # Configurando as layers
     def __init__(self):
         super(Net, self).__init__()
+        # Camadas convolucionais
         self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
         self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        # Zera aleatriamente alguns neuronios da NN
         self.conv2_drop = nn.Dropout2d()
         self.fc1 = nn.Linear(320, 50)
         self.fc2 = nn.Linear(50, 10)
@@ -55,16 +63,14 @@ class Net(nn.Module):
         x = F.dropout(x, training=self.training)
         x = self.fc2(x)
         return F.log_softmax(x)
-
+    
 network = Net()
-optimizer = optim.SGD(network.parameters(), lr=learning_rate,
-                      momentum=momentum)
+optimizer = torch.optim.SGD(network.parameters(), lr=learning_rate,momentum=momentum)
 
 train_losses = []
 train_counter = []
 test_losses = []
 test_counter = [i*len(train_loader.dataset) for i in range(n_epochs + 1)]
-
 
 def train(epoch):
   network.train()
@@ -81,10 +87,10 @@ def train(epoch):
       train_losses.append(loss.item())
       train_counter.append(
         (batch_idx*64) + ((epoch-1)*len(train_loader.dataset)))
+      import os
+      os.makedirs('/results', exist_ok=True)
       torch.save(network.state_dict(), '/results/model.pth')
       torch.save(optimizer.state_dict(), '/results/optimizer.pth')
-
-
 
 def test():
   network.eval()
@@ -93,7 +99,8 @@ def test():
   with torch.no_grad():
     for data, target in test_loader:
       output = network(data)
-      test_loss += F.nll_loss(output, target, size_average=False).item()
+      # Correção: size_average=False foi substituído por reduction='sum'
+      test_loss += F.nll_loss(output, target, reduction='sum').item()
       pred = output.data.max(1, keepdim=True)[1]
       correct += pred.eq(target.data.view_as(pred)).sum()
   test_loss /= len(test_loader.dataset)
@@ -101,8 +108,8 @@ def test():
   print('\nTest set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
     test_loss, correct, len(test_loader.dataset),
     100. * correct / len(test_loader.dataset)))
-  
 
+test()
 for epoch in range(1, n_epochs + 1):
   train(epoch)
   test()
